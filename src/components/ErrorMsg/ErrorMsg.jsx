ed2bae6472200499387d8d75607a326dcf40f2d5
Eden <derhyeden@gmail.com>
2023-05-07 01:07:12 +0300

    deploy

--- src/components/ErrorMsg/ErrorMsg.jsx
+++ src/components/ErrorMsg/ErrorMsg.jsx
@@ -5,14 +5,16 @@ export default function ErrorMsg({ error, callback }) {
 	return (
 		<div className='error-msg grid'>
 			<ErrorSvg />
-			<p>Something went wrong</p>
-			{error && (
+
+			{error ? (
 				<>
 					<span> {error} </span>
 					<button className='btn-try-again' onClick={() => callback()}>
 						Load last search
 					</button>
 				</>
+			) : (
+				<p>Something went wrong</p>
 			)}
 		</div>
 	)
